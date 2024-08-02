e01f6c216b4fe264bba971e0b69154d21c6a4d66
Ebba Grönvall <ebbagronvall@hotmail.com>
2024-08-02 11:49:40 +0000

    Add meta class and strings to posts and comments

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,7 @@ STATUS = ((0, "Draft"), (1, "Published"))
 # Create your models here.
 
 class Post(models.Model):
+   
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
@@ -16,6 +17,13 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"        
+    
 
 
 class Comment(models.Model):
@@ -28,3 +36,9 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
